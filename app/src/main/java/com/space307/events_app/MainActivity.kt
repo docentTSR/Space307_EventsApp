b699c8ee39ee49d16c91ccfc6caf0f5875dd27c0
Anton Savenok <a.savenok@space307com>
2019-12-18 23:38:39 +0300

    repository - add empty project

---
+++ app/src/main/java/com/space307/events_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.space307.events_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
