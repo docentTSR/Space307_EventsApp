2ae10e92a0bf1ab5ceb3cbe8c166f684ba977246
y.shchelokov <y.shchelokov@space307.com>
2019-12-19 17:50:38 +0300

    icons for categories

--- app/src/main/java/com/space307/events_app/EventsAdapter.kt
+++ app/src/main/java/com/space307/events_app/EventsAdapter.kt
@@ -72,6 +72,14 @@ class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private fun getCategoryImage(eventType : EventCategoryType): Int = when(eventType) {
         EventCategoryType.SPORT -> R.drawable.ic_fitness
         EventCategoryType.TRAVEL -> R.drawable.ic_travel
+        EventCategoryType.GAMES -> R.drawable.ic_games
+        EventCategoryType.EDUCATION -> R.drawable.ic_education
+        EventCategoryType.ENTERTAINMENT -> R.drawable.ic_entertainment
+        EventCategoryType.MOVIE -> R.drawable.ic_movie
+        EventCategoryType.DISCUSSIONS -> R.drawable.ic_discussion
+        EventCategoryType.BUSINESS -> R.drawable.ic_business
+        EventCategoryType.FOOD -> R.drawable.ic_food
+
         else -> R.drawable.ic_travel
     }
 }
